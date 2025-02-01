a184bf6aeb9d50cd3d7db4f5be904f91e0ece1de
nouratabbara <nouratabara@gmail.com>
2025-02-01 14:28:37 -0500

    added candlestick chart

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgApexchartsModule } from "ng-apexcharts";
 import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexTitleSubtitle } from 'ng-apexcharts';
+import { CryptoService } from '../../services/crypto.service';
 
 @Component({
   selector: 'app-chart',
@@ -8,27 +9,35 @@ import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexTitleSubtitle } from 'ng
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.scss'
 })
+
 export class ChartComponent {
   public chartOptions: any;
 
-  constructor() {
+  constructor(private cryptoService: CryptoService) {
     this.chartOptions = {
       series: [
         {
-          name: 'Sales',
-          data: [30, 40, 35, 50, 49, 60, 70]
+          name: 'Crypto',
+          data: [
+            { x: new Date('2023-01-01').getTime(), y: [32, 34, 30, 33] },
+            { x: new Date('2023-01-02').getTime(), y: [33, 35, 31, 32] },
+            { x: new Date('2023-01-03').getTime(), y: [32, 33, 30, 31] },
+            { x: new Date('2023-01-04').getTime(), y: [31, 34, 30, 33] },
+            { x: new Date('2023-01-05').getTime(), y: [33, 35, 32, 34] }
+          ]
         }
       ],
       chart: {
         height: 350,
-        type: 'line'
+        type: 'candlestick',
       },
       title: {
         text: 'Product Sales Over Time'
       },
+
       xaxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+        type: 'datetime'
       }
     };
   }
-}
+
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -11,12 +11,12 @@ export class HomePage {
   data: any[] = [];
 
   constructor(private cryptoService: CryptoService) {}
-  
+
   ngOnInit(): void {
     this.cryptoService
       .setSymbol('BTC')
       .setCurrency('USD')
-      .setLimit(90)
+      .setLimit(6)
       .build()
       .subscribe(response => {
         this.data = response.Data.map((item: { close: any; }) => item.close);
